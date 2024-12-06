092ed2f1a4849101633dcffb08ec750c48f33df8
Kerimcan19 <kerocelebi@hotmail.com>
2024-12-06 19:14:55 +0300

    .

--- EF Diyet Programı/Form1.Designer.cs
+++ EF Diyet Programı/Form1.Designer.cs
@@ -57,7 +57,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Diyet Programı";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
